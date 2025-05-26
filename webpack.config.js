bd706ee06bad0fe7632432f162e190b46ca19ff7
Lucas Pozzi - Home <lucaspozzif20@gmail.com>
2025-05-26 19:44:26 -0300

    main - initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/renderer/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'renderer.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+    devtool: 'source-map'
+};
